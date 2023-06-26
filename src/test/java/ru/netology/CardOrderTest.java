418e240b17a12c98b021aca9274e19c4aa5af2a2
Oleg <ovt1966@gmail.com>
2023-06-26 11:06:47 +0300

    Change test

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,7 +1,11 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -10,8 +14,14 @@ class CardOrderTest {
     void shouldCardOrder() {
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").setValue("28.06.23");
+        $("[data-test-id='date'] input").setValue("25.06.23");
         $("[data-test-id='name'] input").setValue("Петров-Водкин Сергей");
+        $("[data-test-id='phone'] input").setValue("+79161111111");
+        $("[data-test-id='agreement']").click();
+        $("button").click();
+
+        $(withText("Успешно!")).shouldBe(Condition.hidden, Duration.ofSeconds(15));
+
 
     }
 
