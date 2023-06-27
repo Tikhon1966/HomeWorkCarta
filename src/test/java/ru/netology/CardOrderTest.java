a8f69335668a3bd038371ea064ae7032b86c064f
Oleg <ovt1966@gmail.com>
2023-06-27 22:36:55 +0300

    Change test2

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,8 +1,7 @@
 package ru.netology;
 
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebElement;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -10,6 +9,7 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.hidden;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -28,20 +28,13 @@ class CardOrderTest {
     void shouldCardOrder() {
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Казань");
-        $("[data-test-id='date'] input").doubleClick().sendKeys("BackSpace");
-        $("[data-test-id='date'] input").setValue(getPossibleData(3));
+        $("[data-test-id='date'] input").doubleClick().setValue(getPossibleData(3));
         $("[data-test-id='name'] input").setValue("Петров-Водкин Сергей");
         $("[data-test-id='phone'] input").setValue("+79161111111");
         $(".checkbox__box").click();
         $(".button").click();
-        WebElement title = $("[data-test-id=notification] .notification__title").shouldBe(visible, Duration.ofSeconds(15));
-        Assertions.assertEquals("Успешно!", title.getText());
-        WebElement content = $("[data-test-id=notification] .notification__content");
-        Assertions.assertEquals("Встреча успешно забронирована на " + getPossibleData(3), content.getText());
-
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldBe(visible, Duration.ofSeconds(15));
 
     }
-
-    ;
-
 }
